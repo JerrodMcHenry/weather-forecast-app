31b0b1c408385f04be2389a3e4fcb22fa984a52e
j <joinjerrodtoday@gmail.com>
2026-04-22 18:17:32 -0700

    Added weather api

---
+++ backend.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+def get_data(place, forecast_days=None, kind=None,):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+if __name__ =="__main__":
+    print(get_data(place="Tokyo"))
